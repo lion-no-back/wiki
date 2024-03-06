bcd52e4bc28972a12e7962d15c90466d763d2ed5
lion-no-back <2265604240@qq.com>
2024-03-06 16:46:40 +0800

    2-7 使用HTTP Client测试接口

---
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.jiawa.wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Songguo
+ * @date 2024/3/6 14:56
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post," + name;
+    }
+}
