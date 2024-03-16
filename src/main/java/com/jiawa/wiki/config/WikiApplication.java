7904fab2bd14976ceb108e0a90597f2ce6fb68c3
lion-no-back <2265604240@qq.com>
2024-03-16 20:25:23 +0800

    5-2 在启动类上增加了@ComponentScan("com.jiawa"),使用axios调用后端接口/ebook/list

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,10 +1,12 @@
-package com.jiawa.wiki;
+package com.jiawa.wiki.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.jiawa")
 @MapperScan("com.jiawa.wiki.mapper")
 public class WikiApplication {
 
