a27a89425450c702538e6c0dfcd4f3b216ca8146
lion-no-back <2265604240@qq.com>
2024-03-17 11:07:44 +0800

    5-4 列表查询接口支持动态SQL，name参数有值时按name查，没值查全部；封面小图样式调整

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -4,6 +4,7 @@ import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
+import com.mysql.cj.util.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -22,7 +23,10 @@ public class EbookService {
     public List<Ebook> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        // 动态SQL
+        if (!StringUtils.isNullOrEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         return ebookMapper.selectByExample(ebookExample);
     }
 }
