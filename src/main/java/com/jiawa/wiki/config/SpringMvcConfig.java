c42e34f4e5a513c8a267a57dbc8bc8a09c29be2d
lion-no-back <2265604240@qq.com>
2024-03-17 12:18:29 +0800

    5-7 增加拦截器，打印接口耗时

---
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,24 @@
+package com.jiawa.wiki.config;
+
+import com.jiawa.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Songguo
+ * @date 2024/3/17 12:10
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
