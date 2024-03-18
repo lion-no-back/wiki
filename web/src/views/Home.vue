cf71651d2531eb7b84f4c98d4aec112feffc1263
lion-no-back <2265604240@qq.com>
2024-03-18 11:03:29 +0800

    6-6 首页显示电子书列表，使用固定分页参数，一次性查出所有电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -75,20 +75,6 @@ import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import axios from 'axios';
 
-const listData: Record<string, string>[] = [];
-
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
 export default defineComponent({
   name: 'Home',
   setup() {
@@ -108,18 +94,19 @@ export default defineComponent({
     ];
 
     onMounted(() => {
-      console.log("onMounted222");
-      axios.get("/ebook/list").then((response) => {
+      axios.get("/ebook/list", {
+        params: {
+          page: 1,
+          size: 1000
+        }
+      }).then((response) => {
         const data = response.data;
-        ebooks.value = data.content;
-        ebooks1.books = data.content;
+        ebooks.value = data.content.list;
       })
     });
 
     return {
       ebooks,
-      ebooks2: toRef(ebooks1, "books"),
-      listData,
       pagination,
       actions,
     }
