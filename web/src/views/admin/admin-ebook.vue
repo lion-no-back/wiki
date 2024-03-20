82d979cfe5b89e808156a0797cd1da4a20b0ced2
lion-no-back <2265604240@qq.com>
2024-03-20 16:10:55 +0800

    6-10 增加删除电子书功能 - 点击删除按钮，弹出确认框，在调用删除接口

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -26,9 +26,16 @@
           <a-button type="primary" @click="edit(record)">
             编辑
           </a-button>
-          <a-button type="danger">
-            删除
-          </a-button>
+          <a-popconfirm
+              title="删除后不可恢复 - 确认删除？"
+              ok-text="是"
+              cancel-text="否"
+              @confirm="handleDelete(record.id)"
+          >
+            <a-button type="danger">
+              删除
+            </a-button>
+          </a-popconfirm>
         </a-space>
       </template>
     </a-table>
@@ -54,7 +61,7 @@
         <a-input v-model:value="ebook.category2Id" />
       </a-form-item>
       <a-form-item label="描述">
-        <a-input v-model:value="ebook.desc" type="text"/>
+        <a-input v-model:value="ebook.description" type="text"/>
       </a-form-item>
     </a-form>
   </a-modal>
@@ -119,10 +126,8 @@ export default defineComponent({
       loading.value = true;
       axios.get("/ebook/list", {
         params: {
-          params: {
-            page: params.page,
-            size: params.size
-          }
+          page: params.page,
+          size: params.size
         }
       }).then((response) => {
         loading.value = false;
@@ -186,6 +191,22 @@ export default defineComponent({
       ebook.value = {};
     }
 
+    /*
+    * 删除
+    * */
+    const handleDelete = (id: number) => {
+      axios.delete("/ebook/delete/" + id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          // 重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize
+          });
+        }
+      })
+    };
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -206,7 +227,9 @@ export default defineComponent({
       ebook,
       modalVisible,
       modalLoading,
-      handleModalOk
+      handleModalOk,
+
+      handleDelete
     }
   }
 });
