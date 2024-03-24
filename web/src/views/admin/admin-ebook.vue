4fd6ab55081b49fead14cdd5a1a4e30eba9141a1
lion-no-back <2265604240@qq.com>
2024-03-24 14:51:55 +0800

    7-7 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -282,10 +282,6 @@ export default defineComponent({
 
     onMounted(() => {
       handleQueryCategory();
-      handleQuery({
-        page: 1,
-        size: pagination.value.pageSize
-      });
     });
 
     return {
