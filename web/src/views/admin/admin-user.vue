fa7ad79993f61b40887ca91abdc5137cf8cb91ec
lion-no-back <2265604240@qq.com>
2024-03-26 18:35:22 +0800

    9-5 密码加密传输和加密存储

--- web/src/views/admin/admin-user.vue
+++ web/src/views/admin/admin-user.vue
@@ -93,6 +93,8 @@ import axios from 'axios';
 import { message } from 'ant-design-vue';
 import { Tool } from '@/util/tool';
 
+declare let hexMd5: any;
+declare let KEY: any;
 
 export default defineComponent({
   name: 'AdminUser',
@@ -172,6 +174,8 @@ export default defineComponent({
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
+
+      user.value.password = hexMd5(user.value.password + KEY);
 
       axios.post("/user/save", user.value).then((response) => {
         modalLoading.value = false;
