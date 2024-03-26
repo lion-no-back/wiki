cc1ace34e3027d1cfbfce7811b1f03f7b30bffec
lion-no-back <2265604240@qq.com>
2024-03-26 14:11:44 +0800

    8-12 点击文档节点时，加载并显示文档内容

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -13,6 +13,7 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div :innerHTML="html"></div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -30,6 +31,7 @@ export default defineComponent({
   setup() {
     const route = useRoute();
     const docs = ref();
+    const html = ref();
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -44,6 +46,28 @@ export default defineComponent({
      */
     const level1 = ref(); // 一级文档树，children属性就是二级文档
     level1.value = [];
+
+    /**
+     * 内容查询
+     **/
+    const handleQueryContent = (id: number) => {
+      axios.get("/doc/find-content/" + id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const onSelect = (selectedKeys: any, info: any) => {
+      console.log('selected', selectedKeys, info);
+      if (Tool.isNotEmpty(selectedKeys)) {
+        // 加载内容
+        handleQueryContent(selectedKeys[0]);
+      }
+    };
 
     /**
      * 数据查询
@@ -68,6 +92,8 @@ export default defineComponent({
 
     return {
       level1,
+      html,
+      onSelect
     }
   }
 });
