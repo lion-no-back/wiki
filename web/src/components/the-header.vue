7c76054542c8a8dad673ab71da7d37470c59ef2e
lion-no-back <2265604240@qq.com>
2024-03-27 15:08:56 +0800

    9-13 增加路由登录拦截，未登录时跳到首页

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -4,20 +4,21 @@
     <a-menu
         theme="dark"
         mode="horizontal"
-        v-model:selectedKeys="selectedKeys1"
         :style="{ lineHeight: '64px' }"
     >
       <a-menu-item key="/"><router-link to="/">首页</router-link></a-menu-item>
-      <a-menu-item key="/admin/user" :style="user.id? {} : {display:'none'}">
+      <a-menu-item key="/admin/user" v-if="user.id">
         <router-link to="/admin/user">用户管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/ebook" :style="user.id? {} : {display:'none'}">
+      <a-menu-item key="/admin/ebook" v-if="user.id">
         <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/admin/category" :style="user.id? {} : {display:'none'}">
+      <a-menu-item key="/admin/category" v-if="user.id">
         <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
-      <a-menu-item key="/about" ><router-link to="/about">关于我们</router-link></a-menu-item>
+      <a-menu-item key="/about">
+        <router-link to="/about">关于我们</router-link>
+      </a-menu-item>
       <a-popconfirm
           title="确认退出登录?"
           ok-text="是"
