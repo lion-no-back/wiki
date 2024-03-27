4167bf2fe4d7b7b16b10f8e09011f130fb0a0c4e
lion-no-back <2265604240@qq.com>
2024-03-27 16:20:30 +0800

    10-2 文档页面显示文档阅读数点赞数

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -15,6 +15,14 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div>
+            <h2>{{doc.name}}</h2>
+            <div>
+              <span>阅读数：{{doc.viewCount}}</span> &nbsp; &nbsp;
+              <span>点赞数：{{doc.voteCount}}</span>
+            </div>
+            <a-divider style="height: 2px; background-color: #9999cc"/>
+          </div>
           <div class="editor-content-view" :innerHTML="html"></div>
         </a-col>
       </a-row>
@@ -36,6 +44,9 @@ export default defineComponent({
     const html = ref();
     const defaultSelectedKeys = ref();
     defaultSelectedKeys.value = [];
+    // 当前选中的文档
+    const doc = ref();
+    doc.value = {};
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -65,14 +76,6 @@ export default defineComponent({
       });
     };
 
-    const onSelect = (selectedKeys: any, info: any) => {
-      console.log('selected', selectedKeys, info);
-      if (Tool.isNotEmpty(selectedKeys)) {
-        // 加载内容
-        handleQueryContent(selectedKeys[0]);
-      }
-    };
-
     /**
      * 数据查询
      **/
@@ -88,11 +91,23 @@ export default defineComponent({
           if (Tool.isNotEmpty(level1)) {
             defaultSelectedKeys.value = [level1.value[0].id];
             handleQueryContent(level1.value[0].id);
+            // 初始显示文档信息
+            doc.value = level1.value[0];
           }
         } else {
           message.error(data.message);
         }
       });
+    };
+
+    const onSelect = (selectedKeys: any, info: any) => {
+      console.log('selected', selectedKeys, info);
+      if (Tool.isNotEmpty(selectedKeys)) {
+        // 选中某一节点时，加载该节点的文档信息
+        doc.value = info.selectedNodes[0].props;
+        // 加载内容
+        handleQueryContent(selectedKeys[0]);
+      }
     };
 
     onMounted(() => {
@@ -103,7 +118,8 @@ export default defineComponent({
       level1,
       html,
       onSelect,
-      defaultSelectedKeys
+      defaultSelectedKeys,
+      doc
     }
   }
 });
