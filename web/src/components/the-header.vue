6a7b8bd4cfc7f05387bd8e18e8f81ba85c5916fd
lion-no-back <2265604240@qq.com>
2024-03-27 14:06:06 +0800

    9-9 vuex整合sessionStorage，解决刷新数据丢失问题

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -53,8 +53,7 @@ export default defineComponent({
   name: 'the-header',
   setup () {
     // 登录后保存
-    const user = ref();
-    user.value = {};
+    const user = computed(() => store.state.user);
 
     // 用来登录
     const loginUser = ref({
@@ -78,7 +77,6 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
-          user.value = data.content;
           store.commit("setUser", data.content);
         } else {
           message.error(data.message);
