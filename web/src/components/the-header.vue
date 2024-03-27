f3281a8ac41519e2644392afa3457074464f1518
lion-no-back <2265604240@qq.com>
2024-03-27 13:42:50 +0800

    9-9 前端显示登录昵称；遗留BUG：昵称无法显示出来；刷新就会没有

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -14,7 +14,10 @@
       <a-menu-item key="/admin/ebook"><router-link to="/admin/ebook">电子书管理</router-link></a-menu-item>
       <a-menu-item key="/admin/category"><router-link to="/admin/category">分类管理</router-link></a-menu-item>
       <a-menu-item key="/about"><router-link to="/about">关于我们</router-link></a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{ user.name }}</span>
+      </a>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal">
         <span>登录</span>
       </a>
     </a-menu>
@@ -48,6 +51,10 @@ declare let KEY: any;
 export default defineComponent({
   name: 'the-header',
   setup () {
+    // 登录后保存
+    const user = ref();
+    user.value = {};
+
     // 用来登录
     const loginUser = ref({
       loginName: "test",
@@ -70,6 +77,7 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功！");
+          user.value = data.content;
         } else {
           message.error(data.message);
         }
@@ -81,7 +89,8 @@ export default defineComponent({
       loginModalLoading,
       showLoginModal,
       loginUser,
-      login
+      login,
+      user
     }
   }
 });
